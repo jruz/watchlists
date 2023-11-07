e89ba619cc0d0297da253485b80e89a698b50d6c
jruz <javi@jruz.io>
2023-11-07 19:55:38 +0100

    binance

---
+++ src/binance.rs
@@ -0,0 +1,82 @@
+use reqwest::header;
+use serde_derive::Deserialize;
+
+#[derive(Deserialize, Debug)]
+pub struct Response {
+    pub symbols: Vec<Symbol>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct Symbol {
+    pub symbol: String,
+    pub status: String,
+    #[serde(rename = "quoteAsset")]
+    pub quote_asset: String,
+    #[serde(rename = "baseAsset")]
+    pub base_asset: String,
+}
+
+const EXCHANGE_NAME: &str = "BINANCE";
+
+async fn get_data() -> Result<Response, serde_json::Error> {
+    let api_url = "https://api.binance.com/api/v3/exchangeInfo?permissions=SPOT".to_string();
+    let client = reqwest::Client::new();
+    let res = client
+        .get(api_url)
+        .header(
+            header::USER_AGENT,
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0",
+        )
+        .send()
+        .await
+        .expect("Failed to get data")
+        .text()
+        .await
+        .expect("Failed to get body");
+
+    //println!("{res:#?}\n");
+    let parsed: Response = serde_json::from_str(&res).expect("Failed to parse JSON");
+
+    Ok(parsed)
+}
+
+pub async fn get_spot() -> Vec<String> {
+    let blacklist = [
+        "TUSD", "USDC", "BUSD", "EUR", "GBP", "PAX", "DAI", "AUD", "USDP",
+    ];
+    let data = get_data().await;
+    match data {
+        Ok(data) => data
+            .symbols
+            .iter()
+            .filter(|row| {
+                row.status == "TRADING"
+                    && row.quote_asset == "USDT"
+                    && !blacklist.contains(&row.base_asset.as_str())
+            })
+            .map(|row| row.symbol.clone())
+            .map(|symbol| EXCHANGE_NAME.to_owned() + ":" + &symbol)
+            .collect(),
+        Err(_) => vec![],
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn integration_get_data() {
+        let data = get_data().await.unwrap();
+
+        assert!(!data.symbols.is_empty());
+    }
+
+    #[tokio::test]
+    async fn integration_get_symbols() {
+        let data = get_spot().await;
+
+        //println!("{data:?}");
+        assert!(!data.is_empty());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufWriter, Write};
 
+mod binance;
 mod woo;
 
 fn write_file(lines: &[String], name: &str) -> io::Result<()> {
@@ -15,23 +16,27 @@ fn write_file(lines: &[String], name: &str) -> io::Result<()> {
     writer.flush()
 }
 
+fn handle_file(data: &[String], name: &str) {
+    match write_file(data, name) {
+        Ok(()) => println!("{name}: {} tickers", &data.len()),
+        Err(e) => println!("Error: {e}"),
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    let perp = woo::get_perp().await;
-    let perp_name = "WOO-PERP";
-    let spot = woo::get_spot().await;
-    let spot_name = "WOO-SPOT";
+    let woo_perp = woo::get_perp().await;
+    let woo_perp_name = "WOO-PERP";
+    let woo_spot = woo::get_spot().await;
+    let woo_spot_name = "WOO-SPOT";
+    let binance_spot = binance::get_spot().await;
+    let binance_spot_name = "BINANCE-SPOT";
 
-    //println!("{perp:#?}\n");
-    //println!("{spot:#?}\n");
+    //println!("{woo_perp:#?}\n");
+    //println!("{woo_spot:#?}\n");
+    //println!("{binance_spot:#?}\n");
 
-    match write_file(&perp, perp_name) {
-        Ok(()) => println!("{perp_name}: {} tickers", &perp.len()),
-        Err(e) => println!("Error: {e}"),
-    }
-
-    match write_file(&spot, spot_name) {
-        Ok(()) => println!("{spot_name}: {} tickers", &spot.len()),
-        Err(e) => println!("Error: {e}"),
-    }
+    handle_file(&woo_perp, woo_perp_name);
+    handle_file(&woo_spot, woo_spot_name);
+    handle_file(&binance_spot, binance_spot_name);
 }
